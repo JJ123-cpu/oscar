0640cb4442df82084da284f6c6f94594eaa20c04
JJ123-cpu <julianjerbaka@gmail.com>
2025-01-19 01:09:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function say(){
+    var elem=document.getElementById("dropdown");
+    if(elem.style.display==="block"){
+       elem.style.display="none";
+    }else{
+       elem.style.display="block";
+    }
+}
